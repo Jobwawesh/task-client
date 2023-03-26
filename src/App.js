5d5d09c2b2a9a6c1aeaa840a89686c48d5ec3c6d
Jobwawesh <jobnjunge15@gmail.com>
2023-03-26 15:56:21 +0300

    Created the login and sign up form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import Login from './components/Login';
+import Navbar from './components/Navbar';
 
 function App() {
+
+  const [token, setToken] = useState()
+  if (!token) {
+    return <Login setToken={setToken}/>
+  }
+
+  
   return (
-    
+    <div className="App">
+      <Navbar />
+
+      <Routes>
+        {/* <Route path='/' element={<Home/>} /> */}
+        {/* <Route path='/profile' element={<Profile/>} /> */}
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
+    </div>
   );
 }
 
